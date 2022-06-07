25607b2ea65d4725f69b664c434165e7d25dbcff
Duygu Koksal <duygukoksalca@gmail.com>
2022-06-07 15:30:08 -0700

    Odev-1 Bootstrap added

---
+++ odev-1(bottstrap)/1.jpeg_files/5b5f63f3835e9e75a829.42.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{KxBF:function(n,o){n.exports=function(n,o,r){var p=-1,w=n.length;o<0&&(o=-o>w?0:w+o),(r=r>w?w:r)<0&&(r+=w),w=o>r?0:r-o>>>0,o>>>=0;for(var a=Array(w);++p<w;)a[p]=n[p+o];return a}}}]);
+//# sourceMappingURL=5b5f63f3835e9e75a829.42.js.map
